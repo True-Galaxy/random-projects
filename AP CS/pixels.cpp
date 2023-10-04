bfe250b7d37d740ae02c4647834c2616eac987ae
Ryan Lagon <rylagon@gmail.com>
2023-10-03 21:43:10 -1000

    added ability to undo

--- AP CS/pixels.cpp
+++ AP CS/pixels.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int dimension, x, y;
     cout << "Please enter the dimension of the grid (Square): "; cin >> dimension;
-    bool grid[dimension][dimension] = {false};
+    bool grid[dimension][dimension] = {0};
     while (true) {
         cout << "\nEnter a coordinate pair, or 0 to exit: ";
         cin >> x;
@@ -12,7 +12,14 @@ int main() {
             break;
         }
         cin >> y;
-        grid[x-1][y-1] = true;
+        if (grid[x-1][y-1] == true) {
+            grid[x-1][y-1] = false;
+        }
+        
+        else {
+            grid[x-1][y-1] = true;
+        }
+        
 
         for (int i = 0; i < dimension;i++) {
             for (int j = 0; j < dimension; j++) {
